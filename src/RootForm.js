3638f45e6b00f0985a1010149c0e2c9e89f84406
Diogo Cruz <diogohmcruz@hotmail.com>
2019-05-13 12:39:19 +0100

    Initial working form

---
+++ src/RootForm.js
@@ -0,0 +1,72 @@
+import React, {Component} from "react";
+import {
+  Button,
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header
+} from "semantic-ui-react";
+
+const factionsOptions = [
+  {key: "mc", value: "mc", text: "Marquise de Cat"},
+  {key: "ed", value: "ed", text: "Eyrie Dynasties"},
+  {key: "wa", value: "wa", text: "Woodland Alliance"},
+  {key: "vb", value: "vb", text: "Vagabond"},
+  {key: "lc", value: "lc", text: "Lizard Cult"},
+  {key: "rc", value: "rc", text: "Riverfolk Company"}
+];
+
+class RootForm extends Component {
+  state = {
+    searchQuery: null,
+    value: [],
+    winner: null
+  };
+
+  handleWinner = (e, {value}) => this.setState({winner: value});
+  handleChangeFactions = (e, {value}) => {
+    var selected_factions = [];
+    value.forEach(v => {
+      const coise = factionsOptions.find(f => f.key === v);
+      selected_factions.push(coise);
+      return coise;
+    });
+    this.setState({value: selected_factions});
+  };
+  handleSearchChange = (e, {searchQuery}) => this.setState({searchQuery});
+
+  toggleSearch = e => this.setState({search: e.target.checked});
+
+  render() {
+    const {played_factions, winner} = this.state;
+
+    return (
+      <Container>
+        <Dropdown
+          fluid
+          selection
+          multiple
+          search
+          options={factionsOptions}
+          value={played_factions}
+          placeholder="Select the Factions"
+          onChange={this.handleChangeFactions}
+          onSearchChange={this.handleSearchChange}
+        />
+        <Dropdown
+          fluid
+          selection
+          search
+          options={this.state.value}
+          value={winner}
+          placeholder="Select the Winner"
+          onChange={this.handleWinner}
+          onSearchChange={this.handleSearchChange}
+        />
+      </Container>
+    );
+  }
+}
+
+export default RootForm;
