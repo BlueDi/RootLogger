5a519f903c0f23a0afa89477ecfdac0bce545ede
Diogo Cruz <diogohmcruz@hotmail.com>
2019-05-13 14:37:24 +0100

    Updates

---
+++ precache-manifest.d6117cc91e41f87a0d3b0574f35eec4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9926950d9de3cd06227fbd99e634dda",
+    "url": "/RootLogger/index.html"
+  },
+  {
+    "revision": "2c0901e7c3e7b65f2f88",
+    "url": "/RootLogger/static/js/2.4c07e198.chunk.js"
+  },
+  {
+    "revision": "72ce039ad829767f5253",
+    "url": "/RootLogger/static/js/main.17a4e88c.chunk.js"
+  },
+  {
+    "revision": "d6644097b833bd9d2638",
+    "url": "/RootLogger/static/js/runtime~main.e716b061.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BlueDi/RootLogger/precache-manifest.4386804274b826606909f572890ffb9e.js"
+  "/RootLogger/precache-manifest.d6117cc91e41f87a0d3b0574f35eec4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BlueDi/RootLogger/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RootLogger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
