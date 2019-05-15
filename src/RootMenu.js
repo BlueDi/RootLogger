78d2773a0539e9bb2c5128c1fada874faacfac1e
Diogo Cruz <diogohmcruz@hotmail.com>
2019-05-15 16:23:52 +0100

    Vast rules link

--- src/RootMenu.js
+++ src/RootMenu.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 
+const rootRulesLink = "http://root.livingrules.io/";
+const vastRulesLink =
+  "https://boardgamegeek.com/filepage/136381/rules-summaries-and-one-pagers";
+
 class RootMenu extends Component {
+  chooseRulesLink() {
+    switch (this.props.game) {
+      case "root":
+        return rootRulesLink;
+      case "vast":
+        return vastRulesLink;
+      default:
+    }
+  }
+
   render() {
     const { game, onSelectGame } = this.props;
 
     return (
       <Menu>
-        <Menu.Item as="a" href="http://root.livingrules.io/" content="Rules" />
+        <Menu.Item
+          as="a"
+          href={this.chooseRulesLink()}
+          target="_blank"
+          content="Rules"
+        />
 
         <Menu.Item
           name="root"
