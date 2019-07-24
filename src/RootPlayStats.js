fb911c228868e4043ad4cc4fc10eaa9e489f6650
Diogo Cruz <diogohmcruz@hotmail.com>
2019-07-24 19:03:10 +0100

    Display graph of Score/Turn

--- src/RootPlayStats.js
+++ src/RootPlayStats.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { Image, Modal } from "semantic-ui-react";
-import { VictoryAxis, VictoryBar, VictoryChart } from "victory";
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryLine } from "victory";
 
 const factionsOptions = require("./staticdata_root").rootFactionsOptions;
 
 class RootPlayStats extends Component {
-  transformPoints() {
-    const { session } = this.props;
-    return Object.keys(session.points).map(key => {
-      const index = factionsOptions.findIndex(f => f.value === key);
-      const faction = factionsOptions[index];
-      return {
-        x: faction.text,
-        y: parseInt(session.points[key], 10),
-        fill: `#${faction.color}`
-      };
-    });
-  }
-
   getWinnerImage(winner) {
     const { session } = this.props;
     const index = factionsOptions.findIndex(f => f.value === session.winner);
     return index >= 0 ? factionsOptions[index].image : null;
+  }
+
+  setWhichGraph() {
+    const { session } = this.props;
+    if (!session.points) return;
+    if (session.points.length > 1) {
+      return <TurnsScoreGraph session={this.props.session} />;
+    } else {
+      return <FinalScoreGraph session={this.props.session} />;
+    }
   }
 
   render() {
@@ -35,38 +32,117 @@ class RootPlayStats extends Component {
             size="medium"
             src={this.getWinnerImage(session.winner)}
           />
-          <Modal.Description>
-            {session.points && (
-              <VictoryChart domainPadding={{ x: 20 }}>
-                <VictoryAxis
-                  style={{
-                    grid: { stroke: "#B3E5FC", strokeWidth: 0.25 }
-                  }}
-                  dependentAxis
-                />
-                <VictoryAxis
-                  style={{
-                    tickLabels: {
-                      padding: 1,
-                      angle: 10,
-                      verticalAnchor: "middle",
-                      textAnchor: "start"
-                    }
-                  }}
-                />
-                <VictoryBar
-                  style={{
-                    data: { fill: d => d.fill }
-                  }}
-                  data={this.transformPoints()}
-                  animate
-                  alignment="start"
-                />
-              </VictoryChart>
-            )}
-          </Modal.Description>
+          <Modal.Description>{this.setWhichGraph()}</Modal.Description>
         </Modal.Content>
       </>
+    );
+  }
+}
+
+class TurnsScoreGraph extends Component {
+  transformPoints() {
+    const { session } = this.props;
+    const points = session.points[0];
+    return Object.keys(points).map(key => {
+      if (key !== "turn") {
+        const index = factionsOptions.findIndex(f => f.value === key);
+        const faction = factionsOptions[index];
+        return session.points.map((turn, index) => {
+          return { x: index + 1, y: turn[key], stroke: `#${faction.color}` };
+        });
+      } else {
+        return null;
+      }
+    });
+  }
+
+  render() {
+    const session_dataset = this.transformPoints();
+    return (
+      <VictoryChart domainPadding={{ x: 20 }}>
+        <VictoryAxis
+          style={{
+            grid: { stroke: "#B3E5FC", strokeWidth: 0.25 }
+          }}
+          dependentAxis
+        />
+        <VictoryAxis
+          style={{
+            tickLabels: {
+              padding: 1,
+              angle: 10,
+              verticalAnchor: "middle",
+              textAnchor: "start"
+            }
+          }}
+        />
+        {session_dataset.map((player_points, index) => {
+          if (player_points != null)
+            return (
+              <VictoryLine
+                key={index}
+                data={player_points}
+                animate
+                alignment="start"
+                style={{
+                  data: { stroke: d => d[0].stroke }
+                }}
+              />
+            );
+          else return null;
+        })}
+      </VictoryChart>
+    );
+  }
+}
+
+class FinalScoreGraph extends Component {
+  transformPoints() {
+    const { session } = this.props;
+    const points = session.points[0];
+    return Object.keys(points).map(key => {
+      if (key !== "turn") {
+        const index = factionsOptions.findIndex(f => f.value === key);
+        const faction = factionsOptions[index];
+        return {
+          x: faction.text,
+          y: parseInt(points[key], 10),
+          fill: `#${faction.color}`
+        };
+      } else {
+        return null;
+      }
+    });
+  }
+
+  render() {
+    return (
+      <VictoryChart domainPadding={{ x: 20 }}>
+        <VictoryAxis
+          style={{
+            grid: { stroke: "#B3E5FC", strokeWidth: 0.25 }
+          }}
+          dependentAxis
+        />
+        <VictoryAxis
+          style={{
+            tickLabels: {
+              padding: 1,
+              angle: 10,
+              verticalAnchor: "middle",
+              textAnchor: "start"
+            }
+          }}
+        />
+        <VictoryBar
+          style={{
+            data: { fill: d => d.fill }
+          }}
+          data={this.transformPoints()}
+          animate
+          alignment="start"
+        />
+      </VictoryChart>
     );
   }
 }
