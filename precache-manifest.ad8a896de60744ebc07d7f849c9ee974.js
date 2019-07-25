808329315e41e56a8d35f0bc11d44ed608d8f934
Diogo Cruz <diogohmcruz@hotmail.com>
2019-07-25 15:54:38 +0100

    Updates

---
+++ precache-manifest.ad8a896de60744ebc07d7f849c9ee974.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fef6b434940f3db62d479f1627e8ea",
+    "url": "/RootLogger/index.html"
+  },
+  {
+    "revision": "d8f495200021dd37e058",
+    "url": "/RootLogger/static/js/2.a73741fc.chunk.js"
+  },
+  {
+    "revision": "22549b6c4f2f24555b1e",
+    "url": "/RootLogger/static/js/main.b8fddd9f.chunk.js"
+  },
+  {
+    "revision": "32dafd9484b8ddaf1c22",
+    "url": "/RootLogger/static/js/runtime~main.6373386f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RootLogger/precache-manifest.5b1173ca4ac135576accc2b81689a9b6.js"
+  "/RootLogger/precache-manifest.ad8a896de60744ebc07d7f849c9ee974.js"
 );
 
 self.addEventListener('message', (event) => {
